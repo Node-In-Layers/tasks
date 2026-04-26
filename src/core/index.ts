09b7c7651a28f3ffcebb07155f1c143872b8cc86
Mike Cornwell <mike@mikecornwell.com>
2026-04-26 13:51:29 -0400

    fix(exports): fix domain exports

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,7 +2,6 @@ import { TasksNamespace } from '../types.js'
 
 export * as services from './services.js'
 export * as features from './features.js'
-export * from './types.js'
 export * as models from './models/index.js'
 
 export const name = TasksNamespace.Core
